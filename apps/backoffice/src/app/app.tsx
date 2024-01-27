0c11a66e0afec8e7450f75e24edc6b838a378663
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-01-27 15:03:27 +0100

    feat(): update root store and app.tsx

--- apps/backoffice/src/app/app.tsx
+++ apps/backoffice/src/app/app.tsx
@@ -1,8 +1,31 @@
 import { Route, Routes } from 'react-router'
 import { ROUTER } from './router.main'
 import { match } from 'ts-pattern'
+import {useGetUserQuery, useLoginMutation, userActions} from "@leadcode/domains/users";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "@leadcode/state/store";
 
 export default function App() {
+  const [login, result] = useLoginMutation()
+  const dispatch = useDispatch<AppDispatch>()
+
+
+
+  useEffect(() => {
+    login({ email: 'nathael.bonnal@gmail.com', password: 'nathael' })
+  }, [])
+
+  useEffect(() => {
+    if (result.status === 'fulfilled') {
+      const { data } = result
+      console.log(result, data)
+
+      dispatch(userActions.setUser(data))
+    }
+
+  }, [result]);
+
   return (
     <div className="text-white">
       <Routes>
--- libs/state/store/src/lib/root.ts
+++ libs/state/store/src/lib/root.ts
@@ -1,6 +1,10 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {userApi, userReducer} from "@leadcode/domains/users";
 
-export const rootReducer = combineReducers({})
+export const rootReducer = combineReducers({
+  user: userReducer,
+  [userApi.reducerPath]: userApi.reducer
+})
 
 export function setupStore(preloadedState?: never) {
   return configureStore({
@@ -10,11 +14,10 @@ export function setupStore(preloadedState?: never) {
       getDefaultMiddleware({
         serializableCheck: true
       })
+        .concat(userApi.middleware)
   })
 }
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = ReturnType<typeof setupStore>
 export type AppDispatch = AppStore['dispatch']
-
-export const initialRootState = (): RootState => ({})

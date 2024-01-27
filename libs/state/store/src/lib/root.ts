0d1cbaa161438a1106105979effc83cd49ac39d9
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-01-27 01:23:26 +0100

    feat(redux): setupStore

--- libs/state/store/src/lib/root.ts
+++ libs/state/store/src/lib/root.ts
@@ -1,3 +1,20 @@
-import {combineReducers} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 
 export const rootReducer = combineReducers({})
+
+export function setupStore(preloadedState?: never) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: true
+      })
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
+
+export const initialRootState = (): RootState => ({})

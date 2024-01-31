ee8824a56825e82497fe948f3d54128597114b2e
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-01-31 23:27:01 +0100

    update modal for create user

--- libs/pages/accounts/src/lib/form/create-user-modal.tsx
+++ libs/pages/accounts/src/lib/form/create-user-modal.tsx
@@ -1,9 +1,11 @@
-import {Controller, FormProvider, useForm} from "react-hook-form";
-import {InputText, ModalCrud} from "@leadcode/ui";
+import {Controller, FormProvider, useForm} from 'react-hook-form'
+import {InputSelect, InputText, ModalCrud} from '@leadcode/ui'
+import { useGetRolesQuery, useStoreUserMutation } from '@leadcode/domains/users'
 
 export interface CreateUserModalProps {
   onClose: () => void
 }
+
 export function CreateUserModal({ onClose }: CreateUserModalProps) {
   const methods = useForm({
     mode: 'onChange',
@@ -17,8 +19,12 @@ export function CreateUserModal({ onClose }: CreateUserModalProps) {
     }
   })
 
+  const { data: rolesResponse } = useGetRolesQuery({ searchParams: 'limit=100' })
+  const [createUser, result] = useStoreUserMutation()
+
   const onSubmit = methods.handleSubmit((data) => {
-    console.log(data)
+    createUser(data)
+    onClose()
   })
 
   return (
@@ -64,6 +70,78 @@ export function CreateUserModal({ onClose }: CreateUserModalProps) {
               />
             )}
           />
+
+          <Controller 
+            name="password"
+            rules={{
+              required: 'Veuillez indiquer une valeur'
+            }}
+            render={({ field, fieldState }) => (
+              <InputText 
+                name="password"
+                value={field.value}
+                onChange={field.onChange}
+                error={fieldState.error?.message}
+                label="Mot de passe"
+                type="password"
+              />
+            )}
+          />
+
+          <div className="grid grid-cols-2 gap-3">
+            <Controller 
+              name="firstname"
+              rules={{
+                required: 'Veuillez indiquer une valeur'
+              }}
+              render={({ field, fieldState }) => (
+                <InputText 
+                  name="firstname"
+                  value={field.value}
+                  onChange={field.onChange}
+                  error={fieldState.error?.message}
+                  label="Prénom"
+                />
+              )}
+            />
+            <Controller 
+              name="lastname"
+              rules={{
+                required: 'Veuillez indiquer une valeur'
+              }}
+              render={({ field, fieldState }) => (
+                <InputText 
+                  name="lastname"
+                  value={field.value}
+                  onChange={field.onChange}
+                  error={fieldState.error?.message}
+                  label="Nom"
+                />
+              )}
+            />
+          </div>
+          {rolesResponse && (
+            <Controller 
+              name="roleIds"
+              render={({ field }) => (
+                <InputSelect 
+                  isSearchable={true}
+                  options={rolesResponse.data.map((role) => {
+                    return {
+                      label: role.label,
+                      value: role.id
+                    }
+                  })}
+                  portal
+                  value={field.value}
+                  onChange={field.onChange}
+                  isMulti
+                  label="Roles"
+                />
+              )}
+            />
+          )}
+         
         </div>
 
       </ModalCrud>
